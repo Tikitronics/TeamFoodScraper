80795522dc1b9f30bc6d446f45767fffdebbe60a
Seilo <meetthedealer@gmail.com>
2019-10-20 22:12:13 +0200

    Erster Commit

---
+++ Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using HtmlAgilityPack;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Text;
+
+namespace TeamFoodCrawler
+{
+    class Program
+    {
+        static string fileName = "TeamFood Speiseplan.txt";
+        static string baseUrl = @"http://www.teamfood.eu/index.php?target=tf/speisekarte_dyn";
+        static string path;
+
+        static void Main(string[] args)
+        {
+            string currentPath;
+
+            Console.OutputEncoding = Encoding.Default;
+            
+            currentPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            path = Path.Combine(currentPath, fileName);
+            File.Delete(path);
+
+            GetAgileMenu();
+
+            Console.ReadLine();
+        }
+
+        static void GetAgileMenu()
+        {
+            List<TeamFoodMenu> menu;
+            string additionalInfo;
+
+            // Hole Webinhalt
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(baseUrl);
+
+            // Hole relevante Informationen aus Webinhalt
+            var contentNode = htmlDoc.GetElementbyId("col3_content");
+
+            additionalInfo = GetAdditionalInfoFromContent(contentNode);
+            menu = GetMenuFromContent(contentNode);
+
+            foreach( var m in menu)
+            {
+                Console.WriteLine(m.ToString());
+                File.AppendAllText(path, m.ToString());
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Zusätze:\n{0}\n{1}", new String('=', 8), additionalInfo);
+
+            Console.WriteLine(sb.ToString());
+            File.AppendAllText(path, sb.ToString());
+        }
+
+        // *****************************************
+        // Hole Zusatzinformationen aus HTML Knoten
+        // *****************************************
+        static string GetAdditionalInfoFromContent(HtmlNode contentNode)
+        {
+            var infos = contentNode.SelectNodes("p");
+
+            if (infos.Count == 0) return "Kein Zusatzinfo gefunden";
+
+            string additionalInfo = infos[0].InnerText;
+            additionalInfo = additionalInfo.Replace("\t", " ");
+            additionalInfo = additionalInfo.Replace("  ", " ");
+
+            return additionalInfo;
+        }
+
+        // *****************************************
+        // Hole Menü aus HTML Knoten
+        // *****************************************
+        static List<TeamFoodMenu> GetMenuFromContent(HtmlNode contentNode)
+        {
+            List<TeamFoodMenu> menu = new List<TeamFoodMenu>();
+            Regex kwPattern = new Regex("(?<=KW )\\d{2}");
+            Regex datePattern = new Regex("\\d{1,2}.\\d{1,2}.\\d{4}");
+
+            NumberStyles style = NumberStyles.AllowDecimalPoint;
+            CultureInfo provider = new CultureInfo("en-US");
+
+            string[] foodTypes = Enum.GetNames(typeof(FoodType));
+
+
+            // Speisepläne
+            var tables = contentNode.SelectNodes("table");
+            var headers = contentNode.SelectNodes("h3");
+
+            if(headers.Count < tables.Count)
+            {
+                throw new Exception("Fehler beim Aufbau der Tabelle (zu wenige Überschriften).");
+            }
+
+            for(int i = 0; i < tables.Count; i++)
+            {
+                DateTime start;
+                DateTime end;
+                int kw;
+
+                // Kalenderwoche
+                string headerText = headers[i].InnerText;
+                var kwMatch = kwPattern.Match(headerText);
+                kw = int.Parse(kwMatch.Value);
+
+                // Start- und Enddatum
+                var dates = datePattern.Matches(headers[i].InnerText);
+                if (dates.Count != 2) throw new Exception("Fehler beim Parsen von Anfangs- und Enddatum");
+                start = DateTime.Parse(dates[0].Value);
+                end = DateTime.Parse(dates[1].Value);
+
+                TeamFoodMenu newMenu = new TeamFoodMenu(kw, start, end);
+
+                // Gehe durch alle Tabellenzeilen
+                var trs = tables[i].SelectNodes("tr");
+                Wochentage currentDay = Wochentage.Montag;
+
+                foreach (var tr in trs)
+                {
+                    var tds = tr.SelectNodes("td");
+
+                    if (tds[0].InnerText == "Tag") continue;
+
+                    TeamFoodMenuItem newMenuItem = new TeamFoodMenuItem();
+
+                    // Extrahiere Wochentag
+                    Wochentage parsedDay;
+                    if (Enum.TryParse<Wochentage>(tds[0].InnerText, out parsedDay))
+                    {
+                        currentDay = parsedDay;
+                    }
+
+                    // Gericht
+                    newMenuItem.Description = WebUtility.HtmlDecode(tds[1].InnerText);
+
+                    // Typ / Fleischart
+                    string type = WebUtility.HtmlDecode(tds[2].InnerText);
+                    FoodType foodType = 0;
+                    
+                    // Gehe alle Namen der FoodTypes durch und füge die hinzu, die im Text vorkommen
+                    for(int t = 0; t < foodTypes.Count(); t++)
+                    {
+                        if (type.Contains(foodTypes[t]))
+                        {
+                            foodType |= (FoodType) Enum.Parse(typeof(FoodType), foodTypes[t], true);
+                            //foodType |= Enum.Parse<FoodType>(foodTypes[t], true);
+                        }
+                    }
+                    // Falls keine FoodTypes im Text gefunden wurden, setze auf "Andere"
+                    if (foodType == 0) foodType = FoodType.Andere;
+                    newMenuItem.Type = foodType;
+
+                    // Preis
+                    string priceString = tds[3].InnerText.TrimEnd('€');
+                    decimal price = decimal.Parse(priceString, style, provider);
+                    newMenuItem.Price = price;
+
+                    // Beilage
+                    string side = WebUtility.HtmlDecode(tds[4].InnerText);
+                    newMenuItem.SideDish = side;
+
+                    // Zusatzstoffe
+                    string additives = tds[5].InnerText;
+                    int[] additivesArray = Array.ConvertAll(additives.Split(','), int.Parse);
+                    newMenuItem.Additives = additivesArray.ToList();
+
+                    newMenu.AddMenuItem(currentDay, newMenuItem);
+                }
+
+                menu.Add(newMenu);
+            }
+
+            return menu;
+        }
+
+    }
+
+
+    
+}
---
+++ TeamFoodMenu.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamFoodCrawler
+{
+    [Flags]
+    public enum FoodType {Andere = 0, Schwein = 1, Rind = 2, Vegetarisch = 4, Geflügel = 8, Fisch = 16};
+    public enum Wochentage { Montag, Dienstag, Mittwoch, Donnerstag, Freitag };
+
+    // *****************************************
+    // Ein Speiseplan für eine Woche
+    // *****************************************
+    class TeamFoodMenu
+    {
+        private int kw;
+        private DateTime startDate;
+        private DateTime endDate;
+        private List<TeamFoodDay> days;
+
+        public List<TeamFoodDay> Days
+        {
+            get { return days; }
+            set { days = value; }
+        }
+
+        public int KW
+        {
+            get { return kw; }
+            set { kw = value; }
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public TeamFoodMenu(int kw, DateTime start, DateTime end)
+        {
+            // Guard Clause
+            if(start == null | end == null | kw > 52)
+            {
+                throw new Exception("Falsche Übergabedaten an Kosturktor zu TeamFoodMenu");
+            }
+
+            KW = kw;
+            StartDate = start;
+            EndDate = end;
+            days = new List<TeamFoodDay>();
+        }
+
+        public void AddMenuItem(Wochentage day, TeamFoodMenuItem menuItem)
+        {
+            if (menuItem == null) throw new Exception("Falsche Daten für MenuItem");
+            if (!Enum.IsDefined(typeof(Wochentage), day)) throw new Exception("Ungültiger Wochentag angegeben.");
+
+            TeamFoodDay teamFoodDay;
+
+            teamFoodDay = days.Find(x => x.Wochentag == day);
+            if (teamFoodDay == null)
+            {
+                teamFoodDay = new TeamFoodDay(day);
+                days.Add(teamFoodDay);
+            }
+
+            teamFoodDay.AddMenuItem(menuItem);
+        }
+
+        // Gibt den Speiseplan als Text aus
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine();
+            sb.AppendFormat("TeamFood Speiseplan für KW {0} ({1} - {2})\n", KW.ToString(), StartDate.ToShortDateString(), EndDate.ToShortDateString());
+            sb.AppendLine(new String('=', 55));
+            sb.AppendLine();
+            foreach (var d in Days)
+            {
+                sb.Append(d.ToString());
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    // *****************************************
+    // Ein Tag bei TeamFood
+    // *****************************************
+    public class TeamFoodDay
+    {
+        private Wochentage weekday;
+        private List<TeamFoodMenuItem> menuItems;
+
+        public Wochentage Wochentag
+        {
+            get { return weekday; }
+            set { weekday = value; }
+        }
+
+        public List<TeamFoodMenuItem> MenuItems
+        {
+            get { return menuItems; }
+            set { menuItems = value; }
+        }
+
+        public TeamFoodDay(Wochentage tag)
+        {
+            if (!Enum.IsDefined(typeof(Wochentage), tag)) throw new Exception("Falscher Wochentag angegeben.");
+            Wochentag = tag;
+            menuItems = new List<TeamFoodMenuItem>();
+        }
+
+        public void AddMenuItem(TeamFoodMenuItem menuItem)
+        {
+            if (menuItem == null ) throw new Exception("Falsche Daten für MenuItem");
+            menuItems.Add(menuItem);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("{0}:\n", weekday);
+            foreach(var mi in menuItems)
+            {
+                sb.Append(mi.ToString());
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    // *****************************************
+    // Ein einzelner Eintrag in die Speiseliste
+    // *****************************************
+    public class TeamFoodMenuItem
+    {
+        private List<int> additives;
+        private string description;
+        private decimal price;
+        private FoodType type;
+        private string sideDish;
+
+        public List<int> Additives
+        {
+            get { return additives; }
+            set { additives = value; }
+        }
+
+
+        public FoodType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set 
+            {
+                description = value.Replace("  ", " "); 
+            }
+        }
+
+        public string SideDish
+        {
+            get { return sideDish; }
+            set { sideDish = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("{0}\n", description);
+            if (SideDish != String.Empty && SideDish != "-") sb.AppendFormat("{0}\n", SideDish);
+            sb.AppendFormat("{0}€ / {1} / ", price, type);
+            var additivesList = string.Join(", ", additives);
+            sb.AppendFormat("Zusätze: {0}\n", additivesList);
+
+            return sb.ToString();
+        }
+    }
+}
